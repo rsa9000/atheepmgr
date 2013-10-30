ea29380f34c8f03b0180a7ec36f9dd4a90a365f6
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2013-10-30 20:42:40 +0400

    eep5211: add partial parsing
    Add support for parsing of the initial part of the EEPROM (PCI
    configuration data, base header, etc.).

--- eep_5211.c
+++ eep_5211.c
@@ -17,11 +17,151 @@
 #include "atheepmgr.h"
 #include "eep_5211.h"
 
+struct eep_5211_priv {
+	struct ar5211_init_eep_data ini;
+	struct ar5211_eeprom eep;
+};
+
+#define EEP_WORD(__off)		le16toh(aem->eep_buf[__off])
+
+static void eep_5211_fill_init_data(struct atheepmgr *aem)
+{
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_init_eep_data *ini = &emp->ini;
+	struct ar5211_pci_eep_data *pci = &ini->pci;
+	struct ar5211_eeprom *eep = &emp->eep;
+	uint16_t word;
+
+	/* Copy data from buffer to PCI initialization structure */
+	memcpy(pci, &aem->eep_buf[AR5211_EEP_PCI_DATA], sizeof(*pci));
+
+	word = EEP_WORD(AR5211_EEP_ENDLOC_UP);
+	ini->eepsz = 2 << (MS(word, AR5211_EEP_ENDLOC_SIZE) + 9);
+	if (word)
+		ini->eeplen = MS(word, AR5211_EEP_ENDLOC_LOC) << 16 |
+			      EEP_WORD(AR5211_EEP_ENDLOC_LO);
+	else
+		ini->eeplen = 0;
+
+	ini->magic = EEP_WORD(AR5211_EEP_MAGIC);
+	ini->prot = EEP_WORD(AR5211_EEP_PROT);
+
+	/* Copy customer data */
+	memcpy(eep->cust_data, &aem->eep_buf[AR5211_EEP_CUST_DATA],
+	       AR5211_EEP_CUST_DATA_SZ * sizeof(uint16_t));
+}
+
+static void eep_5211_fill_headers_30(struct atheepmgr *aem)
+{
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_eeprom *eep = &emp->eep;
+	struct ar5211_base_eep_hdr *base = &eep->base;
+	uint16_t word;
+
+	word = EEP_WORD(AR5211_EEP_ANTGAIN_30);
+	base->antgain_2g = MS(word, AR5211_EEP_ANTGAIN_2G);
+	base->antgain_5g = MS(word, AR5211_EEP_ANTGAIN_5G);
+}
+
+static void eep_5211_fill_headers_33(struct atheepmgr *aem)
+{
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_eeprom *eep = &emp->eep;
+	struct ar5211_base_eep_hdr *base = &eep->base;
+	uint16_t word;
+
+	word = EEP_WORD(AR5211_EEP_ANTGAIN_33);
+	base->antgain_2g = MS(word, AR5211_EEP_ANTGAIN_2G);
+	base->antgain_5g = MS(word, AR5211_EEP_ANTGAIN_5G);
+
+	if (base->version >= AR5211_EEP_VER_4_0) {
+		word = EEP_WORD(AR5211_EEP_MISC0);
+		base->ear_off = MS(word, AR5211_EEP_EAR_OFF);
+		base->xr2_dis = !!(word & AR5211_EEP_XR2_DIS);
+		base->xr5_dis = !!(word & AR5211_EEP_XR5_DIS);
+		base->eepmap = MS(word, AR5211_EEP_EEPMAP);
+
+		word = EEP_WORD(AR5211_EEP_MISC1);
+		base->tgtpwr_off = MS(word, AR5211_EEP_TGTPWR_OFF);
+		base->exists_32khz = !!(word & AR5211_EEP_32KHZ);
+
+		word = EEP_WORD(AR5211_EEP_SRC_INFO0);
+		base->ear_file_ver = MS(word, AR5211_EEP_EAR_FILE_VER);
+		base->eep_file_ver = MS(word, AR5211_EEP_EEP_FILE_VER);
+
+		word = EEP_WORD(AR5211_EEP_SRC_INFO1);
+		base->ear_file_id = MS(word, AR5211_EEP_EAR_FILE_ID);
+		base->art_build_num = MS(word, AR5211_EEP_ART_BUILD);
+	}
+	if (base->version >= AR5211_EEP_VER_5_0) {
+		word = EEP_WORD(AR5211_EEP_MISC4);
+		base->cal_off = MS(word, AR5211_EEP_CAL_OFF);
+
+		word = EEP_WORD(AR5211_EEP_CAPABILITIES);
+		base->comp_dis = !!(word & AR5211_EEP_COMP_DIS);
+		base->aes_dis = !!(word & AR5211_EEP_AES_DIS);
+		base->ff_dis = !!(word & AR5211_EEP_FF_DIS);
+		base->burst_dis = !!(word & AR5211_EEP_BURST_DIS);
+		base->max_qcu = MS(word, AR5211_EEP_MAX_QCU);
+		base->clip_en = !(word & AR5211_EEP_CLIP_EN);
+
+		word = EEP_WORD(AR5211_EEP_REGCAP);
+		base->rd_flags = MS(word, AR5211_EEP_RD_FLAGS);
+	}
+}
+
+static void eep_5211_fill_headers(struct atheepmgr *aem)
+{
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_eeprom *eep = &emp->eep;
+	struct ar5211_base_eep_hdr *base = &eep->base;
+	uint16_t word;
+
+	word = EEP_WORD(AR5211_EEP_MAC + 0);
+	base->mac[4] = (word & 0xff00) >> 8;
+	base->mac[5] = (word & 0x00ff) >> 0;
+	word = EEP_WORD(AR5211_EEP_MAC + 1);
+	base->mac[2] = (word & 0xff00) >> 8;
+	base->mac[3] = (word & 0x00ff) >> 0;
+	word = EEP_WORD(AR5211_EEP_MAC + 2);
+	base->mac[0] = (word & 0xff00) >> 8;
+	base->mac[1] = (word & 0x00ff) >> 0;
+
+	base->regdomain = EEP_WORD(AR5211_EEP_REGDOMAIN);
+	base->checksum = EEP_WORD(AR5211_EEP_CSUM);
+	base->version = EEP_WORD(AR5211_EEP_VER);
+
+	word = EEP_WORD(AR5211_EEP_OPFLAGS);
+	base->amode_en = !!(word & AR5211_EEP_AMODE);
+	base->bmode_en = !!(word & AR5211_EEP_BMODE);
+	base->gmode_en = !!(word & AR5211_EEP_GMODE);
+	base->turbo2_dis = !!(word & AR5211_EEP_TURBO2_DIS);
+	base->turbo5_maxpwr = MS(word, AR5211_EEP_TURBO5_MAXPWR);
+	base->devtype = MS(word, AR5211_EEP_DEVTYPE);
+	base->rfkill_en = !!(word & AR5211_EEP_RFKILL_EN);
+	base->turbo5_dis = !!(word & AR5211_EEP_TURBO5_DIS);
+
+	if (base->version >= AR5211_EEP_VER_3_3) {
+		eep_5211_fill_headers_33(aem);
+	} else if (base->version >= AR5211_EEP_VER_3_0) {
+		eep_5211_fill_headers_30(aem);
+	}
+}
+
 static bool eep_5211_fill(struct atheepmgr *aem)
 {
 	uint16_t endloc_up, endloc_lo;
+	uint16_t magic;
 	int len = 0, addr;
 	uint16_t *buf = aem->eep_buf;
+
+	/* RAW magic reading with subsequent swaping requirement check */
+	if (!EEP_READ(AR5211_EEP_MAGIC, &magic)) {
+		fprintf(stderr, "EEPROM magic read failed\n");
+		return false;
+	}
+	if (bswap_16(magic) == htole16(AR5211_EEPROM_MAGIC_VAL))
+		aem->eep_io_swap = !aem->eep_io_swap;
 
 	if (!EEP_READ(AR5211_EEP_ENDLOC_UP, &endloc_up) ||
 	    !EEP_READ(AR5211_EEP_ENDLOC_LO, &endloc_lo)) {
@@ -30,6 +170,8 @@ static bool eep_5211_fill(struct atheepmgr *aem)
 	}
 
 	if (endloc_up) {
+		endloc_up = le16toh(endloc_up);
+		endloc_lo = le16toh(endloc_lo);
 		len = ((uint32_t)MS(endloc_up, AR5211_EEP_ENDLOC_LOC) << 16) |
 		      endloc_lo;
 		if (len > aem->eepmap->eep_buf_sz) {
@@ -55,20 +197,169 @@ static bool eep_5211_fill(struct atheepmgr *aem)
 
 	aem->eep_len = addr;
 
+	eep_5211_fill_init_data(aem);
+
+	eep_5211_fill_headers(aem);
+
 	return true;
 }
 
 static bool eep_5211_check(struct atheepmgr *aem)
 {
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_init_eep_data *ini = &emp->ini;
+	struct ar5211_eeprom *eep = &emp->eep;
+	struct ar5211_base_eep_hdr *base = &eep->base;
+
+	if (ini->magic != AR5211_EEPROM_MAGIC_VAL) {
+		fprintf(stderr, "Invalid EEPROM Magic 0x%04x, expected 0x%04x\n",
+			ini->magic, AR5211_EEPROM_MAGIC_VAL);
+		return false;
+	}
+
+	if (base->version < AR5211_EEP_VER_3_0) {
+		fprintf(stderr, "Bad EEPROM version 0x%04x (%d.%d)\n",
+			base->version, MS(base->version, AR5211_EEP_VER_MAJ),
+			MS(base->version, AR5211_EEP_VER_MIN));
+		return false;
+	}
+
+	if (base->version >= AR5211_EEP_VER_4_0) {
+		if (base->ear_off > aem->eep_len) {
+			fprintf(stderr, "EAR data offset (0x%04x) points outside the EEPROM\n",
+				base->ear_off);
+			return false;
+		}
+		if (base->tgtpwr_off > aem->eep_len) {
+			fprintf(stderr, "Target power data offset (0x%04x) points outside the EEPROM\n",
+				base->tgtpwr_off);
+			return false;
+		}
+	}
+	if (base->version >= AR5211_EEP_VER_5_0) {
+		if (!base->cal_off) {
+			fprintf(stderr, "Invalid calibration data offset 0x%04x\n",
+				base->cal_off);
+			return false;
+		} else if (base->cal_off > aem->eep_len) {
+			fprintf(stderr, "Calibration data offset (0x%04x) points outside the EEPROM\n",
+				base->cal_off);
+			return false;
+		}
+	}
+
 	return true;
+}
+
+static void eep_5211_dump_init_data(struct atheepmgr *aem)
+{
+#define PR(_token, _fmt, ...)					\
+		printf("%-20s : " _fmt "\n", _token, ##__VA_ARGS__)
+
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_init_eep_data *ini = &emp->ini;
+	struct ar5211_pci_eep_data *pci = &ini->pci;
+	int i;
+
+	EEP_PRINT_SECT_NAME("EEPROM Init data");
+
+	PR("Device ID", "0x%04x", le16toh(pci->dev_id));
+	PR("Vendor ID", "0x%04x", le16toh(pci->ven_id));
+	PR("Class code", "0x%02x", pci->class_code);
+	PR("Sub class code", "0x%02x", pci->subclass_code);
+	PR("Progr interface", "0x%02x", pci->prog_interface);
+	PR("Revision ID", "0x%02x", pci->rev_id);
+	PR("CIS ptr", "0x%08x", le16toh(pci->cis_hi) << 16 |
+				le16toh(pci->cis_lo));
+	PR("Ssys Device ID", "0x%04x", le16toh(pci->ssys_dev_id));
+	PR("Ssys Vendor ID", "0x%04x", le16toh(pci->ssys_ven_id));
+	PR("Max Lat", "0x%02x", pci->max_lat);
+	PR("Min Gnt", "0x%02x", pci->min_gnt);
+	PR("Int Pin", "0x%02x", pci->int_pin);
+	PR("RfSilent GPIO sel", "%u", pci->rfsilent >> 2 & 0x3);
+	PR("RfSilent GPIO pol", "%s", pci->rfsilent & 0x2 ? "high" : "low");
+
+	PR("End of EAR", "0x%08x", ini->eeplen);
+	PR("EEPROM size", "0x%x (%u)", ini->eepsz, ini->eepsz);
+	PR("Magic", "0x%04x", ini->magic);
+	for (i = 0; i < 8; ++i)
+		printf("Region%d access       : %s\n", i,
+		       sAccessType[(ini->prot >> (i * 2)) & 0x3]);
+
+	printf("\n");
+
+#undef PR
+}
+
+static void eep_5211_dump_base(struct atheepmgr *aem)
+{
+#define PR(_token, _fmt, ...)					\
+		printf("%-20s : " _fmt "\n", _token, ##__VA_ARGS__)
+
+	struct eep_5211_priv *emp = aem->eepmap_priv;
+	struct ar5211_eeprom *eep = &emp->eep;
+	struct ar5211_base_eep_hdr *base = &eep->base;
+
+	EEP_PRINT_SECT_NAME("EEPROM Base Header");
+
+	PR("MacAddress", "%02X:%02X:%02X:%02X:%02X:%02X",
+	   base->mac[0], base->mac[1], base->mac[2],
+	   base->mac[3], base->mac[4], base->mac[5]);
+	PR("RegDomain", "0x%04x", base->regdomain);
+	if (base->version >= AR5211_EEP_VER_5_0)
+		PR("RD flags", "0x%04x", base->rd_flags);
+	PR("Checksum", "0x%04x", base->checksum);
+	PR("Version", "%u.%u", MS(base->version, AR5211_EEP_VER_MAJ),
+			       MS(base->version, AR5211_EEP_VER_MIN));
+	PR("RfKill status", "%s", base->rfkill_en ? "enabled" : "disabled");
+	PR("Device Type", "%s", sDeviceType[base->devtype]);
+	PR("Turbo 5GHz status", "%s", base->turbo5_dis ? "disabled" : "enabled");
+	if (base->version >= AR5211_EEP_VER_4_0)
+		PR("Turbo 2GHz status", "%s", base->turbo2_dis ? "disabled" : "enabled");
+	PR(".11a status", "%s", base->amode_en ? "enabled" : "disabled");
+	PR(".11g status", "%s", base->gmode_en ? "enabled" : "disabled");
+	PR(".11b status", "%s", base->bmode_en ? "enabled" : "disabled");
+	if (base->version >= AR5211_EEP_VER_4_0) {
+		PR("XR 5GHz status", "%s", base->xr5_dis ? "disabled" : "enabled");
+		PR("XR 2GHz status", "%s", base->xr2_dis ? "disabled" : "enabled");
+	}
+	PR("Turbo 5G 2W pow, dBm", "%3.1f", (double)base->turbo5_maxpwr / 2);
+	PR("5GHz ant gain, dBm", "%3.1f", (double)base->antgain_5g / 2);
+	PR("2GHz ant gain, dBm", "%3.1f", (double)base->antgain_2g / 2);
+	if (base->version >= AR5211_EEP_VER_4_0) {
+		PR("EEP map", "%u", base->eepmap);
+		PR("EAR offset", "0x%04x", base->ear_off);
+		PR("32kHz crystal", "%s", base->exists_32khz ? "exists" : "no");
+		PR("Target power offset", "0x%04x", base->tgtpwr_off);
+		PR("EEP file version", "%u", base->eep_file_ver);
+		PR("EAR file version", "%u", base->ear_file_ver);
+		PR("EAR file id", "0x%02x", base->ear_file_id);
+		PR("ART build number", "%u", base->art_build_num);
+	}
+	if (base->version >= AR5211_EEP_VER_5_0) {
+		PR("Cal. data offset", "0x%04x", base->cal_off);
+		PR("Comp status", "%s", base->comp_dis ? "disabled" : "enabled");
+		PR("AES status", "%s", base->aes_dis ? "disabled" : "enabled");
+		PR("FF status", "%s", base->ff_dis ? "disabled" : "enabled");
+		PR("Burst status", "%s", base->burst_dis ? "disabled" : "enabled");
+		PR("Max QCU", "%u", base->max_qcu);
+		PR("Allow clipping", "%s", base->clip_en ? "enabled" : "disabled");
+	}
+
+	printf("\n");
+
+#undef PR
 }
 
 const struct eepmap eepmap_5211 = {
 	.name = "5211",
 	.desc = "Legacy .11abg chips EEPROM map (AR5211/AR5212/AR5414/etc.)",
+	.priv_data_sz = sizeof(struct eep_5211_priv),
 	.eep_buf_sz = AR5211_SIZE_MAX,
 	.fill_eeprom = eep_5211_fill,
 	.check_eeprom = eep_5211_check,
 	.dump = {
+		[EEP_SECT_INIT] = eep_5211_dump_init_data,
+		[EEP_SECT_BASE] = eep_5211_dump_base,
 	},
 };
--- eep_5211.h
+++ eep_5211.h
@@ -17,10 +17,28 @@
 #ifndef EEP_5211_H
 #define EEP_5211_H
 
+#define AR5211_EEPROM_MAGIC_VAL		0x5aa5
+
 /* This max value is almost arbitrary and it selection is based on my
  * own observations. */
 #define AR5211_SIZE_MAX			0x2000		/* 16KB EEPROM */
 #define AR5211_SIZE_DEF			0x0400		/* 2KB EEPROM */
+
+#define AR5211_EEP_VER_3_0		0x3000
+#define AR5211_EEP_VER_3_3		0x3003
+#define AR5211_EEP_VER_4_0		0x4000
+#define AR5211_EEP_VER_5_0		0x5000
+
+/**
+ * NB: the EEPROM layout for legacy chips is not byte-aligned, so there are
+ * quite impossible to represent it as a C structure. So we should use here
+ * the same approach as the drivers used: we define a structure that keeps
+ * only the EERPOM data and fill it during the initialization procedure by
+ * processing EEPROM contents in a word-by-word manner. Also, define a set of
+ * macros to facilitate the EEPROM parsing and keep the code a magic-less.
+ */
+
+#define AR5211_EEP_PCI_DATA		0x0000
 
 #define AR5211_EEP_ENDLOC_LO		0x001b		/* Lower word */
 
@@ -29,5 +47,167 @@
 #define AR5211_EEP_ENDLOC_SIZE_S	0
 #define AR5211_EEP_ENDLOC_LOC		0xfff0		/* End location */
 #define AR5211_EEP_ENDLOC_LOC_S		4
+
+#define AR5211_EEP_MAC			0x001d
+
+#define AR5211_EEP_MAGIC		0x003d
+
+#define AR5211_EEP_PROT			0x003f
+
+#define AR5211_EEP_CUST_DATA		0x00b0
+#define AR5211_EEP_CUST_DATA_SZ		0x0f		/* Region size, words */
+
+#define AR5211_EEP_REGDOMAIN		0x00bf
+
+#define AR5211_EEP_INFO_BASE		0x00c0
+
+#define AR5211_EEP_CSUM			(AR5211_EEP_INFO_BASE + 0x00)
+
+#define AR5211_EEP_VER			(AR5211_EEP_INFO_BASE + 0x01)
+#define AR5211_EEP_VER_MAJ		0xf000
+#define AR5211_EEP_VER_MAJ_S		12
+#define AR5211_EEP_VER_MIN		0x0fff
+#define AR5211_EEP_VER_MIN_S		0
+
+#define AR5211_EEP_OPFLAGS		(AR5211_EEP_INFO_BASE + 0x02)
+#define AR5211_EEP_MODES		0x0007
+#define AR5211_EEP_MODES_S		0
+#define AR5211_EEP_AMODE		BIT(0)
+#define AR5211_EEP_BMODE		BIT(1)
+#define AR5211_EEP_GMODE		BIT(2)
+#define AR5211_EEP_TURBO2_DIS		BIT(3)	/* 2GHz turbo disabled */
+#define AR5211_EEP_TURBO5_MAXPWR	0x07f0
+#define AR5211_EEP_TURBO5_MAXPWR_S	4
+#define AR5211_EEP_DEVTYPE		0x3800
+#define AR5211_EEP_DEVTYPE_S		11
+#define AR5211_EEP_RFKILL_EN		BIT(14)
+#define AR5211_EEP_TURBO5_DIS		BIT(15)
+
+#define AR5211_EEP_ANTGAIN_30		(AR5211_EEP_INFO_BASE + 0x04)
+#define AR5211_EEP_ANTGAIN_33		(AR5211_EEP_INFO_BASE + 0x03)
+#define AR5211_EEP_ANTGAIN_2G		0x00ff
+#define AR5211_EEP_ANTGAIN_2G_S		0
+#define AR5211_EEP_ANTGAIN_5G		0xff00
+#define AR5211_EEP_ANTGAIN_5G_S		8
+
+#define AR5211_EEP_MISC0		(AR5211_EEP_INFO_BASE + 0x04)
+#define AR5211_EEP_EAR_OFF		0x0fff
+#define AR5211_EEP_EAR_OFF_S		0
+#define AR5211_EEP_XR2_DIS		BIT(12)
+#define AR5211_EEP_XR5_DIS		BIT(13)
+#define AR5211_EEP_EEPMAP		0xc000
+#define AR5211_EEP_EEPMAP_S		14
+
+#define AR5211_EEP_MISC1		(AR5211_EEP_INFO_BASE + 0x05)
+#define AR5211_EEP_TGTPWR_OFF		0x0fff
+#define AR5211_EEP_TGTPWR_OFF_S		0
+#define AR5211_EEP_32KHZ		BIT(14)
+
+/* aka MISC2 in ath5k */
+#define AR5211_EEP_SRC_INFO0		(AR5211_EEP_INFO_BASE + 0x06)
+#define AR5211_EEP_EAR_FILE_VER		0x00ff
+#define AR5211_EEP_EAR_FILE_VER_S	0
+#define AR5211_EEP_EEP_FILE_VER		0xff00
+#define AR5211_EEP_EEP_FILE_VER_S	8
+
+/* aka MISC3 in ath5k */
+#define AR5211_EEP_SRC_INFO1		(AR5211_EEP_INFO_BASE + 0x07)
+#define AR5211_EEP_EAR_FILE_ID		0x00ff
+#define AR5211_EEP_EAR_FILE_ID_S	0
+#define AR5211_EEP_ART_BUILD		0xfc00
+#define AR5211_EEP_ART_BUILD_S		10
+
+#define AR5211_EEP_MISC4		(AR5211_EEP_INFO_BASE + 0x08)
+#define AR5211_EEP_CAL_OFF		0xfff0
+#define AR5211_EEP_CAL_OFF_S		4
+
+/* aka MISC5 in ath5k */
+#define AR5211_EEP_CAPABILITIES		(AR5211_EEP_INFO_BASE + 0x09)
+#define AR5211_EEP_COMP_DIS		BIT(0)
+#define AR5211_EEP_AES_DIS		BIT(1)
+#define AR5211_EEP_FF_DIS		BIT(2)
+#define AR5211_EEP_BURST_DIS		BIT(3)
+#define AR5211_EEP_MAX_QCU		0x01f0
+#define AR5211_EEP_MAX_QCU_S		4
+#define AR5211_EEP_CLIP_EN		BIT(9)
+
+/* aka MISC5 in ath5k */
+#define AR5211_EEP_REGCAP		(AR5211_EEP_INFO_BASE + 0x0a)
+#define AR5211_EEP_RD_FLAGS		0xffc0
+#define AR5211_EEP_RD_FLAGS_S		6
+
+struct ar5211_pci_eep_data {
+	uint16_t dev_id;
+	uint16_t ven_id;
+	uint8_t subclass_code;
+	uint8_t class_code;
+	uint8_t rev_id;
+	uint8_t prog_interface;
+	uint16_t reserved1;
+	uint16_t cis_hi;
+	uint16_t cis_lo;
+	uint16_t ssys_dev_id;
+	uint16_t ssys_ven_id;
+	uint8_t min_gnt;
+	uint8_t max_lat;
+	uint8_t zeroes1;
+	uint8_t int_pin;
+	uint16_t reserved2;
+	uint16_t pm_cap;
+	uint8_t zeroes2;
+	uint8_t pm_data_scale;
+	uint8_t pm_data_d0;
+	uint8_t pm_data_d3;
+	uint16_t rfsilent;	/* + CLKRUN_EN flag */
+	uint16_t reserved3[11];
+} __attribute__((packed));
+
+struct ar5211_init_eep_data {
+	struct ar5211_pci_eep_data pci;
+	uint32_t eepsz;
+	uint32_t eeplen;
+	uint16_t magic;
+	uint16_t prot;
+};
+
+struct ar5211_base_eep_hdr {
+	uint8_t mac[6];
+	uint16_t regdomain;
+	uint16_t rd_flags;
+	uint16_t checksum;
+	uint16_t version;
+	int amode_en:1;
+	int bmode_en:1;
+	int gmode_en:1;
+	int turbo2_dis:1;
+	int turbo5_dis:1;
+	int rfkill_en:1;
+	int xr2_dis:1;
+	int xr5_dis:1;
+	int exists_32khz:1;
+	int comp_dis:1;
+	int aes_dis:1;
+	int ff_dis:1;
+	int burst_dis:1;
+	int clip_en:1;
+	uint8_t turbo5_maxpwr;
+	uint8_t devtype;
+	uint8_t antgain_2g;
+	uint8_t antgain_5g;
+	uint16_t ear_off;
+	uint8_t eepmap;
+	uint16_t tgtpwr_off;
+	uint8_t eep_file_ver;
+	uint8_t ear_file_ver;
+	uint8_t ear_file_id;
+	uint8_t art_build_num;
+	uint16_t cal_off;
+	uint8_t max_qcu;
+};
+
+struct ar5211_eeprom {
+	struct ar5211_base_eep_hdr base;
+	uint8_t cust_data[AR5211_EEP_CUST_DATA_SZ * 2];
+};
 
 #endif	/* EEP_5211_H */
