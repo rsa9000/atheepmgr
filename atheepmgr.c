81f78deb9ce771969e4b28553936af78e14d089f
Sergey Ryazanov <ryazanov.s.a@gmail.com>
2019-07-01 13:42:52 +0300

    Release 1.2
    Update version information in the usage message.

--- atheepmgr.c
+++ atheepmgr.c
@@ -443,7 +443,7 @@ static void usage(char *name)
 	int i;
 
 	printf(
-		"Atheros NIC EEPROM management utility v1.1\n"
+		"Atheros NIC EEPROM management utility v1.2\n"
 		"Copyright (c) 2008-2011, Atheros Communications Inc.\n"
 		"Copyright (c) 2011-2012, Qualcomm Atheros, Inc.\n"
 		"Copyright (c) 2013-2019, Sergey Ryazanov <ryazanov.s.a@gmail.com>\n"
