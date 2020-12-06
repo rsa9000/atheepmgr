rsa9000/atheepmgr

commit 5596efa8910c33d0a82aba8d8f53546dcad96a46

eep9300: print PAPD rate masks
Print PAPD rate masks from the per-band modal headers.

eep_9300.c
@@ -647,6 +647,8 @@ static void eep_9300_dump_modal_header(struct atheepmgr *aem)
 	PR("TX frame to xpa on", "", "d", pModal->txFrameToXpaOn);
 	PR("TxClip", "", "d", pModal->txClip);
 	PR("ADC Desired Size", "", "d", pModal->adcDesiredSize);
+	PR("PAPD Rate Mask HT20", "0x", "x", pModal->papdRateMaskHt20);
+	PR("PAPD Rate Mask HT40", "0x", "x", pModal->papdRateMaskHt40);
 
 	printf("\n");
 

commit 5c1b6d5fb6b00cfff280649e97dce5aac32489ec

eep9300: fix PAPD mask printing
Mask is multi-byte field, so it should be converted to host endians
before printing.

Fixes: 5596efa ("eep9300: print PAPD rate masks")

eep_9300.c
@@ -487,12 +487,16 @@ static int eep_9300_check(struct atheepmgr *aem)
 		bswap_32_inplace(pModal->antCtrlCommon2);
 		for (i = 0; i < ARRAY_SIZE(pModal->antCtrlChain); ++i)
 			bswap_16_inplace(pModal->antCtrlChain[i]);
+		bswap_32_inplace(pModal->papdRateMaskHt20);
+		bswap_32_inplace(pModal->papdRateMaskHt40);
 
 		pModal = &eep->modalHeader2G;
 		bswap_32_inplace(pModal->antCtrlCommon);
 		bswap_32_inplace(pModal->antCtrlCommon2);
 		for (i = 0; i < ARRAY_SIZE(pModal->antCtrlChain); ++i)
 			bswap_16_inplace(pModal->antCtrlChain[i]);
+		bswap_32_inplace(pModal->papdRateMaskHt20);
+		bswap_32_inplace(pModal->papdRateMaskHt40);
 	}
 
 	return true;