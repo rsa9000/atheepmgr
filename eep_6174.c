rsa9000/atheepmgr

commit d178e35e6edc16c6c3b3382764369addd751238c

Add stub QCA6174 support
Add very basic QCA6174 chips calibration (board) data support. At the
moment utility support only blob (file) loading, basic checking and
very basic base header dumping.

eep_6174.c
@@ -1,0 +1,113 @@
+/*
+ * Copyright (c) 2021 Sergey Ryazanov <ryazanov.s.a@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "atheepmgr.h"
+#include "utils.h"
+#include "eep_common.h"
+#include "eep_6174.h"
+
+struct eep_6174_priv {
+	int curr_ref_tpl;		/* Current reference EEPROM template */
+	struct qca6174_eeprom eep;
+};
+
+static bool eep_6174_load_blob(struct atheepmgr *aem)
+{
+	const int data_size = sizeof(struct qca6174_eeprom);
+	struct eep_6174_priv *emp = aem->eepmap_priv;
+	int res;
+
+	if (aem->con->blob->getsize(aem) < data_size)
+		return false;
+	res = aem->con->blob->read(aem, aem->eep_buf, data_size);
+	if (res != data_size) {
+		fprintf(stderr, "Unable to read calibration data blob\n");
+		return false;
+	}
+
+	memcpy(&emp->eep, aem->eep_buf, sizeof(emp->eep));
+
+	aem->eep_len = (data_size + 1) / 2;
+
+	return true;
+}
+
+static int eep_6174_check(struct atheepmgr *aem)
+{
+	struct eep_6174_priv *emp = aem->eepmap_priv;
+	struct qca6174_eeprom *eep = &emp->eep;
+	struct qca6174_base_eep_hdr *pBase = &eep->baseEepHeader;
+	uint16_t sum;
+
+	if (pBase->length != sizeof(*eep) &&
+	    bswap_16(pBase->length) != sizeof(*eep)) {
+		fprintf(stderr, "Bad EEPROM length 0x%04x/0x%04x (expect 0x%04x)\n",
+			pBase->length, bswap_16(pBase->length),
+			(unsigned int)sizeof(*eep));
+		return false;
+	}
+
+	/**
+	 * NB: take pointer another one time from container to avoid warning
+	 * about a *possible* unaligned access
+	 */
+	sum = eep_calc_csum((uint16_t *)&emp->eep,
+			    sizeof(emp->eep) / sizeof(uint16_t));
+	if (sum != 0xffff) {
+		fprintf(stderr, "Bad EEPROM checksum 0x%04x\n", sum);
+		return false;
+	}
+
+	return true;
+}
+
+static void eep_6174_dump_base_header(struct atheepmgr *aem)
+{
+	const struct eep_6174_priv *emp = aem->eepmap_priv;
+	const struct qca6174_eeprom *eep = &emp->eep;
+	const struct qca6174_base_eep_hdr *pBase = &eep->baseEepHeader;
+
+	EEP_PRINT_SECT_NAME("EEPROM Base Header");
+
+	printf("%-30s : 0x%04X\n", "Length", pBase->length);
+	printf("%-30s : 0x%04X\n", "Checksum", pBase->checksum);
+	printf("%-30s : %d\n", "EEP Version", pBase->eepromVersion);
+	printf("%-30s : %d\n", "Template Version", pBase->templateVersion);
+	printf("%-30s : %02X:%02X:%02X:%02X:%02X:%02X\n",
+	       "MacAddress",
+	       pBase->macAddr[0], pBase->macAddr[1], pBase->macAddr[2],
+	       pBase->macAddr[3], pBase->macAddr[4], pBase->macAddr[5]);
+
+	printf("\nCustomer Data in hex:\n");
+	hexdump_print(pBase->custData, sizeof(pBase->custData));
+
+	printf("\n");
+}
+
+const struct eepmap eepmap_6174 = {
+	.name = "6174",
+	.desc = "EEPROM map for .11ac chips (QCA6174)",
+	.chip_regs = {
+		.srev = 0x08f0,
+	},
+	.priv_data_sz = sizeof(struct eep_6174_priv),
+	.eep_buf_sz = sizeof(struct qca6174_eeprom) / sizeof(uint16_t),
+	.load_blob = eep_6174_load_blob,
+	.check_eeprom = eep_6174_check,
+	.dump = {
+		[EEP_SECT_BASE] = eep_6174_dump_base_header,
+	},
+};

Makefile
@@ -7,6 +7,7 @@ OBJ=\
 	con_stub.o	\
 	eep_5211.o	\
 	eep_5416.o	\
+	eep_6174.o	\
 	eep_9285.o	\
 	eep_9287.o	\
 	eep_9300.o	\

atheepmgr.c
@@ -25,6 +25,7 @@ static struct atheepmgr __aem;
 static const struct eepmap * const eepmaps[] = {
 	&eepmap_5211,
 	&eepmap_5416,
+	&eepmap_6174,
 	&eepmap_9285,
 	&eepmap_9287,
 	&eepmap_9300,
@@ -38,6 +39,8 @@ static const struct eepmap * const eepmaps[] = {
 		AEM_CHIP(__name, &eepmap_5211)
 #define AEM_CHIP_EEP5416(__name)		\
 		AEM_CHIP(__name, &eepmap_5416)
+#define AEM_CHIP_EEP6174(__name)		\
+		AEM_CHIP(__name, &eepmap_6174)
 #define AEM_CHIP_EEP9285(__name)		\
 		AEM_CHIP(__name, &eepmap_9285)
 #define AEM_CHIP_EEP9287(__name)		\
@@ -131,6 +134,10 @@ static const struct chip chips[] = {
 	{ AEM_CHIP_EEP9880("QCA9882"), .pciids = {{ .dev_id = 0x003c }} },
 	{ AEM_CHIP_EEP9880("QCA9890"), .pciids = {{ .dev_id = 0x003c }} },
 	{ AEM_CHIP_EEP9880("QCA9892"), .pciids = {{ .dev_id = 0x003c }} },
+
+	/* QCA6174 EEPROM map PCIe chip(s) */
+	{ AEM_CHIP_EEP6174("QCA6164"), .pciids = {{ .dev_id = 0x0041 }} },	/* Check PCI Id */
+	{ AEM_CHIP_EEP6174("QCA6174"), .pciids = {{ .dev_id = 0x003e }} },
 
 	/* QCA9888 EEPROM map PCIe chip(s) */
 	{ AEM_CHIP_EEP9888("QCA9886"), .pciids = {{ .dev_id = 0x0056 }} },

atheepmgr.h
@@ -282,6 +282,7 @@ extern const struct connector con_stub;
 
 extern const struct eepmap eepmap_5211;
 extern const struct eepmap eepmap_5416;
+extern const struct eepmap eepmap_6174;
 extern const struct eepmap eepmap_9285;
 extern const struct eepmap eepmap_9287;
 extern const struct eepmap eepmap_9300;

eep_6174.h
@@ -1,0 +1,41 @@
+/*
+ * Copyright (c) 2021 Sergey Ryazanov <ryazanov.s.a@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef EEP_6174_H
+#define EEP_6174_H
+
+#define QCA6174_CUSTOMER_DATA_SIZE		20
+
+struct qca6174_base_eep_hdr {
+	uint16_t length;
+	uint16_t checksum;
+	uint8_t eepromVersion;
+	uint8_t templateVersion;
+	uint8_t macAddr[6];
+	uint8_t __unkn_0c[32];
+	uint8_t custData[QCA6174_CUSTOMER_DATA_SIZE];
+} __attribute__ ((packed));
+
+struct qca6174_eeprom {
+	struct qca6174_base_eep_hdr baseEepHeader;
+
+	uint8_t __unkn_0040[8060]; /* to match structure size to the EEPROM data size */
+} __attribute__ ((packed));
+
+/* Structure size watchdog */
+_Static_assert(sizeof(struct qca6174_eeprom) == 8124, "Invalid QCA6174 EEPROM structure size");
+
+#endif

hw.h
@@ -19,6 +19,7 @@
 #define HW_H
 
 #define AR5211_SREV		0x4020
+#define QCA6174_SREV		0x08f0
 #define QCA988X_SREV		0x40ec
 #define QCA9888_SREV		0x800ec
 #define AR_SREV_ID		0x000000FF

commit f84150405639f7493b830bb3d777d2fbc6f7320b

eep6174/9888: fix multibyte fields endians handling
Endians are not accounted for QCA6174/QCA9888 data formats, so we got
wrong length/checksum value on a BigEndian CPUs. Error also should
manifest themself with BigEndian encoded data on LittleEndian CPUs, but
I never saw BigEndian encoded data for these chips.

In order to correctly output multibyte field values utilizes the same
approach as was used for other EEPROM formats (e.g. AR5416, QCA9880,
etc.). Turn field endians to a native CPU endian during data checking
stage, before any prints. We still do not know location of the flag,
that indicates BigEndian encoded data, so assume that data is always
LittleEndian encoded. So we perform endian corrections only on BigEndian
hosts.

Fixes: d178e35 ("Add stub QCA6174 support")
Fixes: 73ecd8c ("Add stub QCA9888 support")

eep_6174.c
@@ -61,6 +61,9 @@ static int eep_6174_check(struct atheepmgr *aem)
 	}
 
 	/**
+	 * Data could come in a compressed form, so calc checksum on the private
+	 * copy of the decomressed calibration data *before* endians fix.
+	 *
 	 * NB: take pointer another one time from container to avoid warning
 	 * about a *possible* unaligned access
 	 */
@@ -69,6 +72,17 @@ static int eep_6174_check(struct atheepmgr *aem)
 	if (sum != 0xffff) {
 		fprintf(stderr, "Bad EEPROM checksum 0x%04x\n", sum);
 		return false;
+	}
+
+	/**
+	 * NB: we still do not know location of the BigEndian format flag, so
+	 * assume that data is always Little Endian.
+	 */
+	if (aem->host_is_be) {
+		printf("EEPROM Endianness is not native.. Changing.\n");
+
+		bswap_16_inplace(pBase->length);
+		bswap_16_inplace(pBase->checksum);
 	}
 
 	return true;

eep_9888.c
@@ -61,6 +61,9 @@ static int eep_9888_check(struct atheepmgr *aem)
 	}
 
 	/**
+	 * Data could come in a compressed form, so calc checksum on the private
+	 * copy of the decomressed calibration data *before* endians fix.
+	 *
 	 * NB: take pointer another one time from container to avoid warning
 	 * about a *possible* unaligned access
 	 */
@@ -69,6 +72,17 @@ static int eep_9888_check(struct atheepmgr *aem)
 	if (sum != 0xffff) {
 		fprintf(stderr, "Bad EEPROM checksum 0x%04x\n", sum);
 		return false;
+	}
+
+	/**
+	 * NB: we still do not know location of the BigEndian format flag, so
+	 * assume that data is always Little Endian.
+	 */
+	if (aem->host_is_be) {
+		printf("EEPROM Endianness is not native.. Changing.\n");
+
+		bswap_16_inplace(pBase->length);
+		bswap_16_inplace(pBase->checksum);
 	}
 
 	return true;